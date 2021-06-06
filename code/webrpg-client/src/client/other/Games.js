8c61ffc863101439050ea9f9dbfef409beb1a3c7
Adam Vivent Kapuściński <a.kapuscinski@thevivent.com>
2021-06-07 00:04:34 +0200

    może nie dokończone, ale jako tako. trzeba zrobić zachowywanie "sesji", żeby testowanie tak nie denerowowało

--- code/webrpg-client/src/client/other/Games.js
+++ code/webrpg-client/src/client/other/Games.js
@@ -1,23 +1,24 @@
-import { Button } from 'bootstrap';
 import { useContext, useEffect, useState } from 'react';
-import { Container, Row, Col, Card } from 'react-bootstrap';
+import { Container, Row, Col, Card, Button, Modal } from 'react-bootstrap';
 import { Redirect } from 'react-router';
 import SocketContext from './SocketContext';
 import SocketMessages from './SocketMessages';
 import UserContext from './UserContext';
 
 const Games = () => {
-	const [state, setState] = useState({ listOfGames: [], loading: true });
+	const [state, setState] = useState({
+		listOfGames: [], // lista gier
+		loading: true, // flaga "czy się ładuję"
+		moreModal: -1, // index gry do wyświetlenia w modalu (-1 oznacza nie wyświetlaj)
+	});
 	const socket = useContext(SocketContext);
 	const [user] = useContext(UserContext);
 
-	if (!user.logged) {
-		return <Redirect to="/" />;
-	}
-
 	const refreshGames = () => {
-		setState({ loading: true });
-		socket.sendJSON(
+        //ustaw flagę "ładuję" na true
+		setState((prevState) => ({ ...prevState, loading: true }));
+		// wyślij prośbę o przesłanie gierek
+        socket.sendJSON(
 			Object.assign(
 				{
 					type: SocketMessages.GET_GAMES,
@@ -27,17 +28,39 @@ const Games = () => {
 		);
 	};
 
-	useEffect(refreshGames, []);
+    // funkcją ustawiająca która gra ma być wyświetlona w modalu
+	const toggleModal = (index) => {
+		setState((prevState) => ({
+			...prevState,
+			moreModal: index,
+		}));
+	};
+
+	useEffect(refreshGames, []); // pobierz gry przy pierwszym załadowaniu
 	useEffect(() => {
+        // przypisz event nasłuchujący zwróconych gier
 		socket.registerOnMessageEvent(
 			SocketMessages.GET_GAMES_RESULT,
 			(msg) => {
-				setState({ listOfGames: msg.games, loading: false });
+				setState((prevState) => ({
+					...prevState,
+					listOfGames: msg.games,
+					loading: false,
+				}));
 			}
 		);
 	}, []);
 
-	return (
+    // jeżeli użytkownik nie jest zalogowany, to
+    // przekieruj na stronę główną
+	if (!user.logged) {
+		return <Redirect to="/" />;
+	}
+
+    // ustaw obecny modal (chodzi, żeby była krótsza zmienna przy wyświetlaniu modalu)
+	var currModal = state.listOfGames[state.moreModal];
+	
+    return (
 		<Container>
 			{state.loading ? (
 				<Row>
@@ -47,24 +70,36 @@ const Games = () => {
 				</Row>
 			) : (
 				<Row xs={1} md={3} xl={5}>
-					{state.listOfGames.map((game) => (
+					{state.listOfGames.map((game, index) => (
 						<Col key={game._id} className="mt-3">
 							<Card style={{ height: '250px' }}>
 								<Card.Header>{game.gameName}</Card.Header>
 								<Card.Body>
 									<Card.Subtitle>{game._id}</Card.Subtitle>
 								</Card.Body>
-                                {/* <Button></Button> */}
+								<Button
+									active
+									variant="primary"
+									onClick={() => toggleModal(index)}
+								>
+									Więcej
+								</Button>
 							</Card>
 						</Col>
 					))}
-					<Col>
-						<Card>
-							<Card.Body></Card.Body>
+					<Col className="mt-3">
+						<Card bg="lime" style={{ height: '250px' }}>
+							<Card.Body>+</Card.Body>
 						</Card>
 					</Col>
 				</Row>
 			)}
+
+			<Modal show={state.moreModal != -1} onHide={() => toggleModal(-1)}>
+				{currModal ? (
+					<Modal.Header>{currModal.gameName}</Modal.Header>
+				) : null}
+			</Modal>
 		</Container>
 	);
 };
